1d33281c87756eb82b2d0060d4e4ea47fd401bd5
yanghyen <sk0310803@naver.com>
2024-06-27 14:16:09 +0900

    ADD: 기부 상세 설명 api

--- donates/urls.py
+++ donates/urls.py
@@ -3,5 +3,6 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 urlpatterns = [
-    path('main/<int:month>/', TopicViewSet.as_view({'get': 'retrieve'}), name='topic-detail-by-month'),
+    path('main/<int:month>/', TopicViewSet.as_view({'get': 'retrieve'}), name='topic-main-by-month'),
+    path('detail/<int:month>/', TopicDetailViewSet.as_view({'get': 'retrieve'}), name='topic-detail-by-month'),
 ]
