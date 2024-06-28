1bf51c575e39a8fb645cb7074417af2ad07bb478
jihee <esder1310@naver.com>
2024-06-28 20:19:40 +0900

    docs:fav.icon setting / theme.js add / modi-index.js

---
+++ src/styles/theme.js
@@ -0,0 +1,27 @@
+const theme={
+    colors:{
+        black:'#333333',
+        white:'#ffffff',
+        greyBlue:'#AFCED6'
+    },
+    backgroundColor:{
+        lightBlue:'#F1FCFF',
+        blue:'#01B8E9',
+        middleBlue:'#0055A5',
+        darkBlue:'#251E74',
+        lightGrey:'#E1F0F4',
+    },
+    font:{
+        english: '"Jersey 15", sans-serif;',
+        korean:'"Noto Sans KR", sans-serif;',
+    },
+    fontSize:{
+        pageHeader:'32px',
+        imgText:'26px',
+        detailText:'18px',
+        gotoDonation:'24px',
+        BtnText:'22px'
+    }
+}
+
+export default theme;
