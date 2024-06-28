a21d052d7de645b6ce495c82a83009668faac2a7
wiseah <hyunah2765@gmail.com>
2024-06-29 00:14:30 +0900

    feat: 현아 중간커밋

--- src/styles/Mobile.js
+++ src/styles/Mobile.js
@@ -1,64 +1,70 @@
-import { Link,Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
+import { BsHouseDoorFill } from "react-icons/bs";
 
-const Container=styled.div`
-    display:flex;
-    justify-content:center;
-    align-items:center;
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     overflow: auto;
-    //height: 100vh; /* Container가 뷰포트의 전체 높이를 차지하도록 설정 */
-`
+`;
 
-const Content=styled.div`
-    position: relative; /* Footer를 절대 위치시키기 위해 Content를 상대 위치로 설정 */
-    width:100%;
-    max-width:428px;
-    min-height:100vh;
-    overflow:auto;
-    box-shadow:0px 0px 32px #0000002f;
+const Content = styled.div`
+    position: relative;
+    width: 100%;
+    max-width: 428px;
+    min-height: 100vh;
+    overflow: auto;
+    box-shadow: 0px 0px 32px #0000002f;
     background-color: #ffffff;
-    //padding-bottom: 260px; //footer높이만큼 패딩 줘야함. 그래야 내용 다보임.
-`
+`;
 
-const Header=styled.div`
-    display:flex;
-    align-items:center;
-    /* justify-content:center; */
-    /* justify-content:space-between; */
-    height:40px;
-    padding:25px;
-`
-const Logo=styled.img`
+const Header = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 40px;
+    padding: 25px;
+`;
+
+const Logo = styled.img`
     width: 135px;
     height: 60px;
-    /* display: flex;
-    flex-direction: row;
-    justify-content: center; */
-`
-const Footer=styled.div`
-    position: absolute; /* Footer를 절대 위치로 설정 */
-    bottom: 0; /* Footer를 Content의 맨 아래에 위치시킴 */
+`;
+
+const Footer = styled.div`
+    position: absolute;
+    bottom: 0;
     width: 100%;
-    display:flex;
-    align-items:center;
-    justify-content:space-between;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     padding-bottom: 26px;
     margin-top: auto;
-`
+`;
 
-const Text=styled.div`
+const Text = styled.div`
     width: 100%;
     text-align: center;
- `   
-const Mobile=()=>{
-    return(
+`;
+
+const Mobile = () => {
+    const location = useLocation();
+    const isHidden = location.pathname === '/' || location.pathname === '/main';
+
+    return (
         <>
             <Container>
                 <Content>
                     <Header>
-                        <Logo src="/images/Logo.png"/>
+                        <Logo src="/images/Logo.png" />
+                        <BsHouseDoorFill 
+                            size='30px' 
+                            color='#0055A5' 
+                            style={{ visibility: isHidden ? 'hidden' : 'visible' }} 
+                        />
                     </Header>
-                    <Outlet /> 
+                    <Outlet />
                     <Footer>
                         <Text>
                             ⓒ 2024. 친환경인도컴퓨터. All rights reserved.
@@ -67,7 +73,7 @@ const Mobile=()=>{
                 </Content>
             </Container>
         </>
-    )
+    );
 };
 
 export default Mobile;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,23 +4,29 @@ const theme={
         white:'#ffffff',
         greyBlue:'#AFCED6'
     },
-    backgroundColor:{
+    backgroundColors:{
         lightBlue:'#F1FCFF',
         blue:'#01B8E9',
         middleBlue:'#0055A5',
         darkBlue:'#251E74',
         lightGrey:'#E1F0F4',
     },
-    font:{
+    fonts:{
         english: '"Jersey 15", sans-serif;',
         korean:'"Noto Sans KR", sans-serif;',
     },
-    fontSize:{
+    fontSizes:{
         pageHeader:'32px',
         imgText:'26px',
         detailText:'18px',
         gotoDonation:'24px',
-        BtnText:'22px'
+        BtnText:'22px',
+        nameText:'18px',
+    },
+    fontWeights:{
+        Bold:'Bold',
+        content:'500',
+        ranking:'Semi Bold',
     }
 }
 
