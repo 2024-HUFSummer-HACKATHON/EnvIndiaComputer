e9512a5fb1f8c57dcd1e0c8b79a72aa50cb9c41b
wiseah <hyunah2765@gmail.com>
2024-06-29 01:21:16 +0900

    feat: 이번달 명예의 전당, 명예의 전당 리스트

--- src/pages/PastRankingList.jsx
+++ src/pages/PastRankingList.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { BsFillStarFill, BsCheckCircleFill, BsDashCircle } from "react-icons/bs";
+import React from 'react';
+import { BsFillStarFill, BsCheckCircleFill, BsDashCircle, BsFillTrophyFill } from "react-icons/bs";
 import styled from 'styled-components';
 import BlockButtons from '../components/BlockButtons';
 
@@ -8,15 +8,18 @@ const Title = styled.div`
   flex-direction: row;
   align-items: center;
   margin: 35px 48px;
-  .icons{
+  font-size: ${({ theme }) => theme.fontSizes.BtnText};
+  font-weight: 600;
+  .icons {
     margin-right: 16px;
     width: 24px;
     height: 24px;
   }
-`
+`;
+
 const List = styled.div`
   padding-bottom: 44px;
-`
+`;
 
 const months = [
   "1월", "2월", "3월", "4월", "5월", "6월", 
@@ -29,27 +32,30 @@ function PastRankingList() {
   return (
     <>
       <Title>
-        <BsFillStarFill className='icons'/>
-        명예의 전당
+        <BsFillTrophyFill className='icons'/>
+        <span style={{color: '#0055A5', marginRight: '6px'}}>2024 </span> 명예의 전당
       </Title>
       <List>
         {months.map((month, index) => {
-          const isPast = index <= currentMonth; // 현재 달 포함
-          const icon = isPast ? <BsCheckCircleFill/> : <BsDashCircle/>;
-          const bgColor = isPast ? '#FFFBF1' : '#EFE7D4';
+          const isPast = index < currentMonth; // 과거 달
+          const isCurrent = index === currentMonth; // 현재 달
+          const icon = isCurrent ? <BsCheckCircleFill/> : <BsDashCircle/>;
+          const bgColor = isPast ? '#01B8E9' : isCurrent ? '#0055A5' : '#E1F0F4';
+          const textColor = isPast ? '#ffffff' : isCurrent ? '#ffffff' : '#AFCED6'; // 현재 달은 흰색(#ffffff), 과거 달은 흰색(#ffffff), 미래 달은 #AFCED6
+
           return (
             <BlockButtons 
               key={index} 
               icon={icon} 
               text={`${month}: ${month} 주제`} 
-              bgColor={bgColor} 
+              bgColor={bgColor}
+              textColor={textColor}
             />
           );
         })}
       </List>
-      
     </>
-  )
+  );
 }
 
-export default PastRankingList
+export default PastRankingList;
