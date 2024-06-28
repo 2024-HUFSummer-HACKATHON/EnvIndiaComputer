0c45d20d9175a077a2554eefa63963ca66afea16
jihee <esder1310@naver.com>
2024-06-29 05:03:02 +0900

    docs: 세부 속성 조정

--- src/styles/Mobile.js
+++ src/styles/Mobile.js
@@ -1,4 +1,4 @@
-import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { BsHouseDoorFill } from "react-icons/bs";
 
@@ -7,6 +7,9 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
     overflow: auto;
+
+    flex-wrap: wrap;
+    gap:1vw;
 `;
 
 const Content = styled.div`
@@ -29,7 +32,6 @@ const Header = styled.div`
 
 
 const Logo = styled.img`
-
     width: 135px;
     height: 60px;
 `;
@@ -37,18 +39,22 @@ const Logo = styled.img`
 const Footer = styled.div`
     position: absolute;
     bottom: 0;
+    display: flex;
+
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: space-between;
-    padding-bottom: 26px;
-    margin-top: auto;
+    padding:1vw 0vw;
+    
 `;
 
 const Text = styled.div`
     width: 100%;
     text-align: center;
 
+    font-weight: 500;
+    color:${({theme})=>theme.colors.black};
 `;
 
 const Mobile = () => {
